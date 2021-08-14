263fdf6bc4ce1384835f8bbfa7ace505054cc2a1
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2021-08-15 00:12:40 +0530

    Update main.js

--- demos/threejs/rupy_helmet/main.js
+++ demos/threejs/rupy_helmet/main.js
@@ -4,7 +4,7 @@
 const SETTINGS = {
   gltfModelURL: 'Head_gear.glb',
   cubeMapURL: 'Bridge2/',
-  offsetYZ: [0,0.01, 0], // offset of the model in 3D along vertical and depth axis
+  offsetYZ: [0,0.1, 0], // offset of the model in 3D along vertical and depth axis
   scale: 1.75
 };
 
