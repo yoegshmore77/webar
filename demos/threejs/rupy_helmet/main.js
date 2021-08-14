96a5cbeee77da4a663cc6ba768e18eba7695dd31
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2021-08-14 23:54:16 +0530

    Update main.js

--- demos/threejs/rupy_helmet/main.js
+++ demos/threejs/rupy_helmet/main.js
@@ -5,7 +5,7 @@ const SETTINGS = {
   gltfModelURL: 'Head_gear.glb',
   cubeMapURL: 'Bridge2/',
   offsetYZ: [0,0.3, 0], // offset of the model in 3D along vertical and depth axis
-  scale: 1.65
+  scale: 1.75
 };
 
 // some globalz:
@@ -46,7 +46,8 @@ function init_threeScene(spec) {
     
     // center and scale the object:
     const bbox = new THREE.Box3().expandByObject(gltf.scene);
-    gltf.scene.rotation.x = -92;
+    //=================================================================
+    gltf.scene.rotation.x = -90;
     //gltf.scene.rotation.z= 90;
     
     
